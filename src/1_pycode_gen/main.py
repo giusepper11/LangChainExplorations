f74337e21caa5921c44d4f2056d122dd00381b59
Giuseppe Rosa <giusepper11@gmail.com>
2023-12-18 20:03:50 -0300

    LangChain, PromptTemplate, LLMChain, SequentialChain

--- src/1_pycode_gen/main.py
+++ src/1_pycode_gen/main.py
@@ -1,27 +1,67 @@
+# Import necessary libraries
+import argparse
 from dotenv import load_dotenv
 from langchain.llms.openai import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Initialize argument parser
+parser = argparse.ArgumentParser()
 
+# Add arguments for task and language with default values
+parser.add_argument("--task", type=str, default="return a list of numbers")
+parser.add_argument("--language", type=str, default="python")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Initialize OpenAI with a specific model
 llm = OpenAI(model_name="gpt-3.5-turbo-1106")
 
+# Define a code prompt template
 code_prompt = PromptTemplate(
     template="Write a very short {language} function that {task}.",
     input_variables=["language", "task"],
 )
 
+# Define a test prompt template
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} function:\n{code}",
+    input_variables=["language", "code"],
+)
+
+# Define a chain for generating code
 code_chain = LLMChain(
     llm=llm,
     prompt=code_prompt,
+    output_key="code",
 )
 
+# Define a chain for generating test
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test",
+)
 
-result = code_chain({
-    "language": "python",
-    "task": "prints 'Hello World!'"
-})
+# Define a sequential chain that includes both code and test chains
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"],
+)
 
-print(result["text"])
+# Generate code using the code chain and print it
+result = chain(
+    {
+        "language": args.language,
+        "task": args.task,
+    }
+)
+
+print(result["code"])
+print("------------------")
+print(result["test"])
